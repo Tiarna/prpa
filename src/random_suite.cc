527cd55796ad1fff0990e52327b6c08823111ff4
Nicolas Allemoniere <allemo_n@epita.fr>
2014-07-13 19:24:16 +0200

    rnadom_suite written

---
+++ src/random_suite.cc
@@ -0,0 +1,40 @@
+#include "random_suite.hh"
+
+static std::vector<int>
+random_uniform_suite(int px_min, int px_max, int size)
+{
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(px_min, px_max);
+  std::vector<int> suite;
+
+  for (int i = 0; i <= size - 1; i++)
+    suite.push_back(distribution(generator));
+
+  // repetitions elimination
+  auto it = suite.begin();
+  while (it != suite.end())
+  {
+    while (std::find(it + 1, suite.end(), *it) != suite.end())
+      *it = (*it + 1) % px_max;
+
+    it++;
+  }
+
+  return suite;
+}
+
+
+std::vector<int> get_random_suite(int px_nb, int sample_nb)
+{
+  if (sample_nb > px_nb)
+  {
+    std::cout << "Too much samples. Only " << px_nb
+      << " samples will be extracted" << std::endl;
+
+    sample_nb = px_nb;
+  }
+
+  std::vector<int> suite = random_uniform_suite(0, px_nb - 1, sample_nb);
+
+  return suite;
+}
---
+++ src/random_suite.hh
@@ -0,0 +1,16 @@
+#ifndef RANDOM_SUITE_HH
+# define RANDOM_SUITE_HH
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <iterator>
+#include <algorithm>
+#include <cstdlib>
+
+std::vector<int> get_random_suite(int px_nb, int sample_nb);
+
+
+#endif /* !RANDOM_SUITE_HH */
+
